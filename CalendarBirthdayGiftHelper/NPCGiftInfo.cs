8eaffa0b699c8dc3e2e494ec1ab62ca4ec07fe54
tyler staples <tyler.a.staples@gmail.com>
2016-03-26 01:15:51 -0700

    added basic util stuff. created class to represent an npc's gift info

---
+++ CalendarBirthdayGiftHelper/NPCGiftInfo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using StardewModdingAPI;
+
+namespace CalendarBirthdayGiftHelper
+{
+    public class NPCGiftInfo
+    {
+        // TODO: make sure no item uses this id
+        public const int INVALID_ITEM_ID = 0;
+
+        private string npcName;
+        private int[] favouriteGifts;
+        private int[] goodGifts;
+
+        public string Name { get { return npcName; } }
+        public int[] FavouriteGifts { get { return favouriteGifts; } }
+        public int[] GoodGifts { get { return goodGifts; } }
+
+        public NPCGiftInfo(string name, string[] favourite, string[] good)
+        {
+            npcName = name;
+            favouriteGifts = Utils.StringToIntArray(favourite);
+            goodGifts = Utils.StringToIntArray(good);
+        }
+    }
+}
---
+++ CalendarBirthdayGiftHelper/Utils.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using StardewModdingAPI;
+
+namespace CalendarBirthdayGiftHelper
+{
+    public class Utils
+    {
+        public static int[] StringToIntArray(string[] array, int defaultVal=0)
+        {
+            int[] output = new int[array.Length];
+            for (int i = 0; i < array.Length; ++i)
+            {
+                try
+                {
+                    output[i] = Int32.Parse(array[i]);
+                }
+                catch (Exception ex)
+                {
+                    Log.Error("[CalendarBirthdayGiftHelper] failed to convert {0} to int32: {1}", array[i], ex);
+                    output[i] = defaultVal;
+                }
+            }
+            return output;
+        }
+    }
+}
