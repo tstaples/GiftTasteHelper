0f03092dec9f186e89a2500862a3abedd4c7b1b2
tyler staples <tyler.a.staples@gmail.com>
2016-03-26 01:40:30 -0700

    npcgift info now stored in local list for later lookup. added logic so it's not done more than once a day

--- CalendarBirthdayGiftHelper/CalendarBirthdayGiftHelper.cs
+++ CalendarBirthdayGiftHelper/CalendarBirthdayGiftHelper.cs
@@ -13,6 +13,11 @@ namespace CalendarBirthdayGiftHelper
 {
     public class CalendarBirthdayGiftHelper : Mod
     {
+        public const int NUM_DAYS = 28;
+
+        private List<NPCGiftInfo> dayGiftInfo; // Indexed by day
+        private int dayInitializedOn = 0;
+
         public override void Entry(params object[] objects)
         {
             MenuEvents.MenuChanged += OnClickableMenuChanged;
@@ -24,36 +29,44 @@ namespace CalendarBirthdayGiftHelper
         {
             DebugPrintMenuInfo(e.PriorMenu, e.NewMenu);
 
-            //MouseState state = Mouse.GetState();
+            if (e.NewMenu == null || !(e.NewMenu is Billboard))
+                return;
 
-            // Get all the birthdays n shit
-            // then listen to mouse move event and check for hover over different day
-            // if mouse on day that has a birthday, render tooltip with gift info
+            // This has already been run today so the info won't have changed
+            if (dayInitializedOn == Game1.dayOfMonth)
+                return;
+
+            // Create our map and note when we did it
+            dayGiftInfo = new List<NPCGiftInfo>(NUM_DAYS);
+            dayInitializedOn = Game1.dayOfMonth;
+
+            // Get the calendar and npc gift taste info
             const string calendarDaysFieldName = "calendarDays";
-            if (e.NewMenu != null && e.NewMenu is Billboard)
+            Billboard calendar = (Billboard)e.NewMenu; // Yes, the calendar is stuffed in the billboard ;-;
+            FieldInfo calendarDaysInfo = typeof(Billboard).GetField(calendarDaysFieldName, BindingFlags.Instance | BindingFlags.NonPublic);
+            List<ClickableTextureComponent> calendarDays = (List<ClickableTextureComponent>)calendarDaysInfo.GetValue(calendar);
+            Dictionary<string, string> npcGiftTastes = Game1.NPCGiftTastes;
+
+            int dayNumber = 1;
+            foreach (ClickableTextureComponent day in calendarDays)
             {
-                Billboard calendar = (Billboard)e.NewMenu; // Yes, the calendar is stuffed in the billboard ;-;
-                FieldInfo calendarDaysInfo = typeof(Billboard).GetField(calendarDaysFieldName, BindingFlags.Instance | BindingFlags.NonPublic);
-                List<ClickableTextureComponent> calendarDays = (List<ClickableTextureComponent>)calendarDaysInfo.GetValue(calendar);
-                Dictionary<string, string> npcGiftTastes = Game1.NPCGiftTastes;
+                string hoverText = day.hoverText;
+                if (hoverText.Length == 0 || !hoverText.Contains("Birthday"))
+                    continue;
 
-                foreach (ClickableTextureComponent day in calendarDays)
+                string npcName = hoverText.Split(new char[] { '\'', ' ' })[0];
+                if (npcGiftTastes.ContainsKey(npcName))
                 {
-                    string hoverText = day.hoverText;
-                    if (hoverText.Length == 0 || !hoverText.Contains("Birthday"))
-                        continue;
+                    string[] giftTastes = npcGiftTastes[npcName].Split(new char[] { '/' });
+                    string[] favouriteGifts = giftTastes[1].Split(new char[] { ' ' });
+                    string[] goodGifts = giftTastes[3].Split(new char[] { ' ' });
 
-                    string npcName = hoverText.Split(new char[] { '\'', ' ' })[0];
-                    if (npcGiftTastes.ContainsKey(npcName))
-                    {
-                        string[] giftTastes = npcGiftTastes[npcName].Split(new char[] { '/' });
-                        string[] favouriteGifts = giftTastes[1].Split(new char[] { ' ' });
-                        string[] goodGifts = giftTastes[3].Split(new char[] { ' ' });
+                    dayGiftInfo[dayNumber] = new NPCGiftInfo(npcName, favouriteGifts, goodGifts);
 
-                        Log.Verbose("Favourite gifts for {0}: {1}", npcName, Utils.ArrayToString(favouriteGifts));
-                        Log.Verbose("Good gifts for {0}: {1}", npcName, Utils.ArrayToString(goodGifts));
-                    }
+                    Log.Verbose("Favourite gifts for {0}: {1}", npcName, Utils.ArrayToString(favouriteGifts));
+                    Log.Verbose("Good gifts for {0}: {1}", npcName, Utils.ArrayToString(goodGifts));
                 }
+                ++dayNumber;
             }
         }
 
