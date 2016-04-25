b723b2aa159a1c57bd47662f7c31cce97dc59086
Vanna Phong <vphong@ucsd.edu>
2016-04-25 13:01:06 -0700

    social page detection cleanup

--- GiftTasteHelper/SocialPageGiftHelper.cs
+++ GiftTasteHelper/SocialPageGiftHelper.cs
@@ -62,11 +62,14 @@ namespace GiftTasteHelper
                 {
                     Rectangle mouseRect = new Rectangle(Mouse.GetState().X, Mouse.GetState().Y, 1, 1);
                     int slotPos = Utils.GetNativeField<int, SocialPage>(soc, "slotPosition");
-                    int drawBoundsWidth = gameMenu.xPositionOnScreen + gameMenu.width - 9 * Game1.pixelZoom;
-                    int drawBoundsHeight = friendSlots[slotPos + 4].bounds.Y + (friendSlots[slotPos + 1].bounds.Y - friendSlots[slotPos].bounds.Y) * 5;
-                    Rectangle drawBounds = new Rectangle(friendSlots[slotPos].bounds.X, friendSlots[slotPos].bounds.Y, drawBoundsWidth, drawBoundsHeight);
+                    int minX = friendSlots[slotPos].bounds.X;
+                    int minY = friendSlots[slotPos].bounds.Y;
+                    int maxX = gameMenu.xPositionOnScreen + gameMenu.width - 9 * Game1.pixelZoom;
+                    int maxY = friendSlots[slotPos + 4].bounds.Y + (friendSlots[slotPos + 1].bounds.Y - friendSlots[slotPos].bounds.Y);
 
-                    if (mouseRect.Intersects(drawBounds))
+                    bool mouseWithinSocialPage = (mouseRect.X > minX && mouseRect.Y > minY && mouseRect.X < maxX && mouseRect.Y < maxY);
+
+                    if (mouseWithinSocialPage)
                     {
                         foreach (ClickableTextureComponent friend in friendSlots)
                         {
