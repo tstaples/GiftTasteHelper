7b5e6dda2a0f219508bf844074f9a7b52e916f5a
Jesse Plamondon-Willard <github@jplamondonw.com>
2017-04-28 20:31:17 -0400

    remove a few more unneeded utils

--- GiftTasteHelper/Framework/Utils.cs
+++ GiftTasteHelper/Framework/Utils.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Diagnostics;
-using System.Reflection;
 using Microsoft.Xna.Framework;
 using StardewModdingAPI;
 
@@ -42,38 +41,11 @@ namespace GiftTasteHelper.Framework
             int[] output = new int[array.Length];
             for (int i = 0; i < array.Length; ++i)
             {
-                if (array[i] == null || !Utils.IsInt32(array[i]))
-                    continue;
-
-                try
-                {
-                    output[i] = Int32.Parse(array[i]);
-                }
-                catch (Exception ex)
-                {
-                    Utils.DebugLog("failed to convert " + array[i] + "to int32: " + ex, LogLevel.Warn);
-                    output[i] = defaultVal;
-                }
+                int value;
+                if (int.TryParse(array[i], out value))
+                    output[i] = value;
             }
             return output;
-        }
-
-        public static bool IsType<T>(object o)
-        {
-            if (o != null)
-                return o.GetType() == typeof(T);
-            return false;
-        }
-
-        public static int GetTileSheetIndexFromID(int id)
-        {
-            if (id == 0)
-                return 0;
-
-            const int spriteSize = 16; // each sprite on this sheet is 16x16
-            int x = (int)Math.Floor((float)(id / 24.0f));
-            int y = id % spriteSize;
-            return (y * spriteSize) + x;
         }
 
         public static int Clamp(int val, int min, int max)
@@ -89,12 +61,6 @@ namespace GiftTasteHelper.Framework
             if (parts.Length > 0)
                 name = parts[0];
             return name;
-        }
-
-        public static bool IsInt32(string s)
-        {
-            int i;
-            return int.TryParse(s, out i);
         }
 
         public static Rectangle MakeRect(float x, float y, float width, float height)
