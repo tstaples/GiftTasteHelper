13326e1511f3baa65830eed6034f57a084b31900
tyler staples <tyler.a.staples@gmail.com>
2017-07-23 19:29:14 -0700

    Re-organizing project files into view/model/controller.

---
+++ GiftTasteHelper/Framework/Models/GiftDatabaseModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace GiftTasteHelper.Framework
+{
+    internal class GiftEntry
+    {
+        public int ItemId { get; set; }
+    }
+
+    internal class GiftDatabaseModel
+    {
+        public Dictionary<string, GiftEntry> Entries { get; set; }
+    }
+}
